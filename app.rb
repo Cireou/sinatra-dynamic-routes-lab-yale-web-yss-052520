4839dbf6cdb5c0ea33f87728d816d8add9f87559
Eric Ou <eric.ou@yale.edu>
2020-06-14 21:08:07 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,41 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get  '/reversename/:name' do 
+    "#{params[:name].reverse()}"
+  end
 
+  get '/square/:number' do 
+    "#{params[:number].to_i ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    "#{params[:phrase] * params[:number].to_i}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1] + " " + 
+      params[:word2] + " " + 
+      params[:word3]+ " " +
+      params[:word4] + " " + 
+      params[:word5] +
+      "."}"
+  end 
+
+  get '/:operation/:number1/:number2' do 
+    "#{process(params[:operation], params[:number1].to_i, params[:number2].to_i)}"
+  end
+
+  def process(operation_string, num_1, num_2)
+    case operation_string
+    when "add"
+      return num_1 + num_2
+    when "subtract"
+      return num_1 - num_2
+    when "multiply"
+      return num_1 * num_2
+    else 
+      return num_1 / num_2
+    end
+  end
 end
